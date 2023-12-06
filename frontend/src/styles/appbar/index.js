a86a67532be54772edb50a4b683e55623255a4e9
OsimFavour <famos204@gmail.com>
2023-12-06 16:22:09 +0100

    Create Styles Folder

---
+++ frontend/src/styles/appbar/index.js
@@ -0,0 +1,49 @@
+import { List, Typography } from "@mui/material"
+import { styled } from "@mui/material/styles"
+import { Box } from "@mui/system"
+import { Colors } from "../theme"
+import '@fontsource/montez'
+
+// Container
+export const AppbarContainer = styled(Box)(() => ({
+    display: 'flex',
+    marginTop: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '2px 8px'
+})) 
+
+
+export const AppbarHeader = styled(Typography)(() => ({
+    padding: "4px",
+    flexGrow: 1,
+    fontSize: "4em",
+    fontFamily: '"Montez", "cursive"',
+    color: Colors.secondary,
+    // "&:hover": {
+    //   animation: `${textPopUpTop} 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both`,
+    // },
+}))
+
+export const MyList = styled(List)(({ type }) => ({
+    display: type === "row" ? "flex" : "block",
+    flexGrow: 3,
+    justifyContent: "center",
+    alignItems: "center",
+}))
+
+export const ActionsIconsContainerMobile = styled(Box)(() => ({
+    display: 'flex',
+    background: Colors.shaft,
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    alignItems: 'center',
+    zIndex: 99,  
+    borderTop: `1px solid ${Colors.border}`
+  }))
+  
+  export const ActionsIconsContainerDesktop = styled(Box)(() => ({
+    flexGrow: 0,
+  }))
