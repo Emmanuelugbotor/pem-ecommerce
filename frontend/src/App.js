228c1ff75bd172713c5ede79ab05a7c90daa284e
OsimFavour <famos204@gmail.com>
2023-12-06 17:01:58 +0100

    New Commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
-import { Button } from '@mui/material'
-import { useEffect } from 'react';
+import { ThemeProvider } from '@mui/system'
+import { Button, Container } from '@mui/material'
+import { useEffect } from 'react'
+import theme from './styles/theme'
+
+// COMPONENETS
+import Appbar from './Components/Appbar'
 
 function App() {
 
   useEffect(() => {
     document.title = 'Ecommerce Home'
-  })
+  }, [])
+
   return (
-    <div className="App">
-      <Button>Test</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='xl' sx={{background: '#fff'}}>
+        <Appbar/>
+        {/* Appbar
+            Banner
+            Promotions
+            Title
+            Products
+            Footer
+            Searchbox
+            Appdrawer */}
+        <Button variant='contained'>Test</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
