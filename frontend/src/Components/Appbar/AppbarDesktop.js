4c7aea3c24127135bcbd356b3d524046b81875b0
OsimFavour <famos204@gmail.com>
2023-12-07 18:58:52 +0100

    Refactor Code

--- frontend/src/Components/Appbar/AppbarDesktop.js
+++ frontend/src/Components/Appbar/AppbarDesktop.js
@@ -1,15 +1,21 @@
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { ListItemButton, ListItemIcon, ListItemText, ListItem } from "@mui/material"
 import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar"
 import SearchIcon from "@mui/icons-material/Search"
 import Actions from "./Actions"
+import "@fontsource/noto-sans"
 
 function AppbarDesktop({ matches }) {
+
+    const listItemTextStyle = {
+        fontFamily: '"Noto Sans" "cursive"',
+        fontWeight: 600,
+    }
 
     return (
         <AppbarContainer>
             <AppbarHeader>Male Clothings</AppbarHeader>
                 <MyList type='row'>
-                    <ListItemText primary='Home'/>
+                    <ListItemText disableTypography sx={listItemTextStyle} primary='Home'/>
                     <ListItemText primary='Categories'/>
                     <ListItemText primary='Products'/>
                     <ListItemText primary='Contact Us'/>
