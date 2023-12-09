fa23df02ffda333a3f476c32f684206cc1b9e4a4
OsimFavour <famos204@gmail.com>
2023-12-09 23:09:53 +0100

    Create Drawer Component

---
+++ frontend/src/Components/AppDrawer/index.js
@@ -0,0 +1,72 @@
+import { styled } from '@mui/system'
+import { lighten } from "polished"
+import CloseIcon from "@mui/icons-material/Close"
+import { Divider, Drawer, List, ListItemButton, ListItemText } from '@mui/material'
+import { useUIContext } from '../../Context/ui'
+import { DrawerCloseButton } from '../../styles/appbar'
+import { Colors } from "../../styles/theme"
+
+
+const MiddleDivider = styled((props) => (
+  <Divider variant='middle' {...props} />
+))``
+
+
+function AppDrawer() {
+
+  const { drawerOpen, setDrawerOpen } = useUIContext()
+  
+  return (
+    <> 
+      {drawerOpen && (
+        <DrawerCloseButton onClick={() => setDrawerOpen(false)}>
+          <CloseIcon
+            className='testing'
+            sx={{
+              fontSize: "2.5rem",
+              color: lighten(0.09, Colors.secondary),
+            }}
+          />
+        </DrawerCloseButton>
+      )}
+
+    <Drawer open={drawerOpen}>
+
+      <List>
+        <ListItemButton>
+          <ListItemText>Home</ListItemText>
+        </ListItemButton>
+
+        <MiddleDivider/>
+
+        <ListItemButton>
+          <ListItemText>Categories</ListItemText>
+        </ListItemButton>
+
+        <MiddleDivider/>
+
+        <ListItemButton>
+          <ListItemText>Products</ListItemText>
+        </ListItemButton>
+
+        <MiddleDivider/>
+
+        <ListItemButton>
+          <ListItemText>About Us</ListItemText>
+        </ListItemButton>
+
+        <MiddleDivider/>
+
+        <ListItemButton>
+          <ListItemText>Contact Us</ListItemText>
+        </ListItemButton>
+
+        <MiddleDivider/>
+      </List>
+
+    </Drawer>
+    </>
+  )
+}
+
+export default AppDrawer
