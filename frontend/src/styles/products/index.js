82bc5a803320a20a9c0fe849227a1990a6945788
OsimFavour <famos204@gmail.com>
2023-12-09 23:16:10 +0100

    Update Product themes

--- frontend/src/styles/products/index.js
+++ frontend/src/styles/products/index.js
@@ -31,8 +31,10 @@ export const ProductActionButton = styled(IconButton)(() => ({
   margin: 4,
 }));
 
-export const ProductFavButton = styled(ProductActionButton)(({ isfav, theme }) => ({
-  color: isfav ? Colors.primary : Colors.light,  
+export const ProductFavButton = styled(ProductActionButton, {
+  shouldForwardProp: (prop) => prop !== "isFav",
+} )(({ isFav, theme }) => ({
+  color: isFav ? Colors.primary : Colors.light,  
   [theme.breakpoints.up("md")]: {
     position: "absolute",
     right: 0,
@@ -58,14 +60,16 @@ export const ProductAddToCart = styled(Button, {
   opacity: 0.9,
 }));
 
-export const ProductMetaWrapper = styled(Box)(({theme}) => ({
+export const ProductMetaWrapper = styled(Box)(() => ({
   padding: 4,
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
 }));
 
-export const ProductActionsWrapper = styled(Box)(({ show, theme }) => ({ 
+export const ProductActionsWrapper = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "show",
+})(({ show, theme }) => ({ 
   [theme.breakpoints.up("md")]: {
     display: show ? 'visible' : 'none',
     position: "absolute",
