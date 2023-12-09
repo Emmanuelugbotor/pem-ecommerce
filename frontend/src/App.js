4484e73801e012537da919d1bec9ec847e43377f
OsimFavour <famos204@gmail.com>
2023-12-09 23:29:04 +0100

    Add Footer, AppDrawer Component, Add UIContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { ThemeProvider } from '@mui/system'
-import { Button, Container, Typography, Box } from '@mui/material'
+import { Container, Typography, Box } from '@mui/material'
 import { useEffect } from 'react'
 import theme from './styles/theme'
 
@@ -8,27 +8,40 @@ import Appbar from './Components/Appbar'
 import Banner from './Components/Banner'
 import Promotions from './Components/Promotions'
 import Products from './Components/Products/'
+import Footer from './Components/Footer'
+import AppDrawer from './Components/AppDrawer'
+import { UIProvider } from './Context/ui'
 
 function App() {
 
   useEffect(() => {
-    document.title = 'Ecommerce Home'
+    document.title = 'E-Commerce'
   }, [])
 
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth='xl' sx={{background: '#fff'}}>
-        
-        <Appbar/>
-        <Banner/>
-        <Promotions/>
-        <Box display='flex' justifyContent='center' sx={{ p:4 }}>
-          <Typography variant='h4'>
-            Our Products
-          </Typography>
-        </Box>
-        <Products/>
 
+        <UIProvider>
+          <Appbar/>
+
+          <Banner/>
+
+          <Promotions/>
+
+          <Box display='flex' justifyContent='center' sx={{ p:4 }}>
+            <Typography variant='h4'>
+              Product Features
+            </Typography>
+          </Box>
+
+          <Products/>
+
+          <Footer/>
+
+          <AppDrawer/>
+        </UIProvider>
+        
       </Container>
     </ThemeProvider>
   );
