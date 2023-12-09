9d656b2fda81065e45ac9889182e113207e1d815
OsimFavour <famos204@gmail.com>
2023-12-09 23:19:48 +0100

    Change Product Image

--- frontend/src/Data/index.js
+++ frontend/src/Data/index.js
@@ -4,41 +4,41 @@ export const products = [
         name: "Super Italian Wear",
         price: 129.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_01.png"
+        image: "/images/products/image_01.png"
     },
     {
         id: 2,
         name: "New Hip Wears",
         price: 199.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_02.png"
+        image: "/images/products/image_02.png"
     },
     {
         id: 3,
         name: "Elite Series",
         price: 189.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_03.png"
+        image: "/images/products/image_03.png"
     },
     {
         id: 4,
         name: "Casual",
         price: 129.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_04.png"
+        image: "/images/products/image_04.png"
     },
     {
         id: 5,
         name: "Best Tote",
         price: 399.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_05.png"
+        image: "/images/products/image_05.png"
     },
     {
         id: 6,
         name: "Charming Series",
         price: 689.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/cloth_06.png"
+        image: "/images/products/image_06.png"
     }
 ]
