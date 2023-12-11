3a5c0108be55ac98b2184433b31b8088dfa605c3
OsimFavour <famos204@gmail.com>
2023-12-11 22:08:47 +0100

    Add Dialog Hook

--- frontend/src/Components/Products/SingleProduct.js
+++ frontend/src/Components/Products/SingleProduct.js
@@ -4,28 +4,39 @@ import ProductMeta from "./ProductMeta"
 import ShareIcon from "@mui/icons-material/Share"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import FitScreenIcon from "@mui/icons-material/FitScreen"
+import useDialogModal from "../../Hooks/useDialogModal"
+import ProductDetail from "../ProductDetail"
 
 function SingleProduct({product, matches}) {
+
+    const [ProductDetailDialog, showProductDetailDialog, closeProductDetailDialog] = useDialogModal(ProductDetail)
+
     return (
         <>
         <Product>
             <ProductImage src={product.image}/>
+
             <ProductMeta product={product} matches={matches}/>
+
             <ProductActionsWrapper>
                 <Stack direction='row'>
                     <ProductFavButton isFav={0}>
                         <FavoriteIcon/>
                     </ProductFavButton>
+
                     <ProductActionButton>
                         <ShareIcon color='primary'/>
                     </ProductActionButton>
-                    <ProductActionButton>
+                    
+                    <ProductActionButton onClick={() => showProductDetailDialog()}>
                         <FitScreenIcon color='primary'/>
                     </ProductActionButton>
                 </Stack>
             </ProductActionsWrapper>
         </Product>
+
         <ProductAddToCart variant="contained">Add To Cart</ProductAddToCart>
+        <ProductDetailDialog product={product} />
         </>
     )
 }
