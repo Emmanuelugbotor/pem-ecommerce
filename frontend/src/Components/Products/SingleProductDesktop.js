396c04b7a1f1b1e8cdda535eec6f54bcaba77749
OsimFavour <famos204@gmail.com>
2023-12-11 22:08:18 +0100

    Add Dialog Hook

--- frontend/src/Components/Products/SingleProductDesktop.js
+++ frontend/src/Components/Products/SingleProductDesktop.js
@@ -5,11 +5,16 @@ import ProductMeta from "./ProductMeta"
 import ShareIcon from "@mui/icons-material/Share"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import FitScreenIcon from "@mui/icons-material/FitScreen"
+import useDialogModal from "../../Hooks/useDialogModal"
+import ProductDetail from "../ProductDetail"
 
 
 function SingleProductDesktop({product, matches}) {
 
     const [showOptions, setShowOptions] = useState(false)
+
+    const [ProductDetailDialog, showProductDetailDialog, closeProductDetailDialog] = useDialogModal(ProductDetail)
+
 
     const handleMouseEnter = () => {
         setShowOptions(true)
@@ -22,26 +27,33 @@ function SingleProductDesktop({product, matches}) {
         <>
         <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <ProductImage src={product.image}/>
+
             <ProductFavButton isFav={0}>
                 <FavoriteIcon/>
             </ProductFavButton>
+
             {showOptions && (
                 <ProductAddToCart show={showOptions} variant="contained">
                     Add To Cart
                 </ProductAddToCart>
             )}
+
             <ProductActionsWrapper show={showOptions}>
                 <Stack direction='column'>
                     <ProductActionButton>
                         <ShareIcon color='primary'/>
                     </ProductActionButton>
-                    <ProductActionButton>
+
+                    <ProductActionButton onClick={() => showProductDetailDialog()}>
                         <FitScreenIcon color='primary'/>
                     </ProductActionButton>
                 </Stack>
             </ProductActionsWrapper>
+
         </Product>  
+
         <ProductMeta product={product} matches={matches}/>
+        <ProductDetailDialog product={product} />
         </>
     )
 }
