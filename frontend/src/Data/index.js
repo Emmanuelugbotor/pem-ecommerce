285fe0d3c2f70a7bdd6efb454431a5250a486e41
OsimFavour <famos204@gmail.com>
2023-12-15 11:52:53 +0100

    Update Product data

--- frontend/src/Data/index.js
+++ frontend/src/Data/index.js
@@ -36,9 +36,345 @@ export const products = [
     },
     {
         id: 6,
-        name: "Charming Series",
+        name: "Charming Boys",
         price: 689.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/image_06.png"
+    },
+    {
+        id: 7,
+        name: "Super Italian Wear for Boys",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_07.png"
+    },
+    {
+        id: 8,
+        name: "New Hip Wears for Boys",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_08.png"
+    },
+    {
+        id: 9,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_09.png"
+    },
+    {
+        id: 10,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_10.png"
+    },
+    {
+        id: 11,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_11.png"
+    },
+    {
+        id: 12,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_12.png"
+    },
+    {
+        id: 13,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_13.png"
+    },
+    {
+        id: 14,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_14.png"
+    },
+    {
+        id: 15,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_01.png"
+    },
+    {
+        id: 16,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_02.png"
+    },
+    {
+        id: 17,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_03.png"
+    },
+    {
+        id: 18,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_04.png"
+    },
+    {
+        id: 19,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_05.png"
+    },
+    {
+        id: 20,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_06.png"
+    },
+    {
+        id: 21,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_07.png"
+    },
+    {
+        id: 22,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_08.png"
+    },
+    {
+        id: 23,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_09.png"
+    },
+    {
+        id: 24,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_10.png"
+    },
+    {
+        id: 25,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_11.png"
+    },
+    {
+        id: 26,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_12.png"
+    },
+    {
+        id: 27,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_13.png"
+    },
+    {
+        id: 28,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_14.png"
+    },
+    {
+        id: 29,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_01.png"
+    },
+    {
+        id: 30,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_02.png"
+    },
+    {
+        id: 31,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_03.png"
+    },
+    {
+        id: 32,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_04.png"
+    },
+    {
+        id: 33,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_05.png"
+    },
+    {
+        id: 34,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_06.png"
+    },
+    {
+        id: 35,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_07.png"
+    },
+    {
+        id: 36,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_08.png"
+    },
+    {
+        id: 37,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_09.png"
+    },
+    {
+        id: 38,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_10.png"
+    },
+    {
+        id: 39,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_11.png"
+    },
+    {
+        id: 40,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_13.png"
+    },
+    {
+        id: 41,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_14.png"
+    },
+    {
+        id: 42,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_01.png"
+    },
+    {
+        id: 43,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_02.png"
+    },
+    {
+        id: 44,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_03.png"
+    },
+    {
+        id: 45,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_04.png"
+    },
+    {
+        id: 46,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_05.png"
+    },
+    {
+        id: 47,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_06.png"
+    },
+    {
+        id: 48,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_07.png"
+    },
+    {
+        id: 49,
+        name: "Super Italian Wear",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_08.png"
+    },
+    {
+        id: 50,
+        name: "New Hip Wears",
+        price: 199.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_09.png"
+    },
+    {
+        id: 51,
+        name: "Elite Series",
+        price: 189.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_10.png"
+    },
+    {
+        id: 52,
+        name: "Casual",
+        price: 129.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_11.png"
+    },
+    {
+        id: 53,
+        name: "Best Tote",
+        price: 399.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_12.png"
+    },
+    {
+        id: 54,
+        name: "Charming Series",
+        price: 689.99,
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/image_13.png"
     }
 ]
